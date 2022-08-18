d60747065469cc1829244091a1f5ef60b052c80f
jonathan_briceño <brimanz2014@gmail.com>
2022-08-17 20:38:02 -0400

    destructuring part one

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,34 @@
     ===== Código de TypeScript =====
 */
 
+interface Reproductor{
+    volumen: number,
+    segundo: number,
+    cancion: string,
+    detalles: Detalles
+}
+
+interface Detalles{
+    author: string,
+    anio: number
+}
+
+const reproductor: Reproductor = {
+    volumen: 90,
+    segundo: 48,
+    cancion: "Somethin about us",
+    detalles: {
+        author: "Daft punk",
+        anio: 2000
+    }
+}
+
+const {volumen, segundo, cancion, detalles} = reproductor;
+const {author} = detalles;
+
+console.log("El volumen es: ", volumen);
+console.log("El segundo es: ", segundo);
+console.log("La cancion es: ", cancion);
+console.log("El author es: ", author );
+
+//segunda parte de la clase
